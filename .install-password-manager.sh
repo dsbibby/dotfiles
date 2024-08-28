b79f2bce4a0aeb774f19f15afd2c96a18bf4b1e8
David <david@Davids-MacBook-Air.local>
2024-08-28 13:57:54 +0100

    Add .chezmoi.yaml Add .chezmoidata/packages.yaml Add .install-password-manager.sh Update .bash_profile Add .test_brewfile Add darwin-install-packages.sh
    Update .chezmoi.yaml

---
+++ .install-password-manager.sh
@@ -0,0 +1,25 @@
+#!/bin/sh
+
+# exit immediately if bws is already in $PATH
+type bws >/dev/null 2>&1 && exit
+
+[ "$(uname -m)" = "x86_64" ] && arch="x86_64" || arch="aarch64"
+# Get Latest Release
+latest_version=$(curl -s https://raw.githubusercontent.com/bitwarden/sdk/main/crates/bws/Cargo.toml | grep -o '^version = ".*"' | grep -Eo "[0-9]+\.[0-9]+\.[0-9]+")
+
+case "$(uname -s)" in
+Darwin)
+    asset_name="bws-${arch}-apple-darwin-${latest_version}.zip"
+    ;;
+Linux)
+    asset_name="bws-${arch}-unknown-linux-gnu-${latest_version}.zip"
+    ;;
+*)
+    echo "unsupported OS"
+    exit 1
+    ;;
+esac
+
+curl -o bws-cli.zip -Ls "https://github.com/bitwarden/sdk/releases/latest/download/${asset_name}"
+mkdir ~/.local/bin
+unzip bws-cli.zip -d ~/.local/bin
